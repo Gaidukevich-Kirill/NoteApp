c12277a363f969be0d70fc57a084732cc8c27ae8
Kiryl <kirya_gaydukevich@mail.ru>
2025-07-30 00:28:28 +0300

    delete calendar, +fields for sorting by date

--- frontend/my-notes/src/components/Filters.tsx
+++ frontend/my-notes/src/components/Filters.tsx
@@ -1,12 +1,49 @@
 import * as React from "react";
 import './Filters.css';
+import { useEffect, useState } from "react";
 
 export default function Filters({ filter, setFilter }: any) {
+  const [startSearchDate, setStartSearchDate] = useState();
+  const [endSearchDate, setEndSearchDate] = useState();
+
+  const handleStartDateChange = (event: any) => {
+    setStartSearchDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event: any) => {
+    setEndSearchDate(event.target.value);
+  };
+
+
   return (
     <div>
+      <h1 className="">Фильтры</h1>
+
       <div>
         <input
-          placeholder="Поиск"
+          className="dateSearch"
+          type="date"
+          id="start"
+          min="2000-01-01"
+          max="2099-12-31"
+          value={startSearchDate}
+          onChange={handleStartDateChange}
+        ></input>
+
+        <input
+          className="dateSearch"
+          type="date"
+          id="start"
+          min="2000-01-01"
+          max="2099-12-31"
+          value={endSearchDate}
+          onChange={handleEndDateChange}
+        ></input>
+
+      </div>
+      <div>
+        <input
+          placeholder="Поиск по заголовку"
           onChange={(e) => setFilter({ ...filter, search: e.target.value })}
         />
       </div>
